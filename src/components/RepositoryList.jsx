07e7b3b379620946418c8d6a2f6f0475860714e9
viictorcamposs <campos.viictor@gmail.com>
2021-03-25 18:11:21 -0300

    feat: consumindo api de repositorios do github

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/viictorcamposs/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Repository List</h1>
+
+      <ul>
+        {repositories.map(repository => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
+      </ul>
+    </section>
+  );
+};
